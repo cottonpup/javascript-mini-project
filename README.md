45e84d7d453489dc02747f0cc5bf93fc19e1ada1
Younggyoung <younggyoung.lee1>
2020-11-04 21:31:12 +0900

    Update js

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ HTML doc 과 자바스크립트 코드와의 connecting point.
 
 Document: Special object that is the entry point to the DOM.
 
+Math object, random Method
+
--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 'use strict';
 
-document.querySelector('.check').addEventListener('click', function(){
-   const guess = document.querySelector('.guess').value;
-   
-   if(!guess){
-       document.querySelector('.message').textContent = '⚠️ No number!';
-   }
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⚠️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎊 Correct number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '😭 Too high';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '😩 Too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 });
