5a9b0ab94a4f0f82f256c8fb57c5896c471619e9
Younggyoung <younggyoung.lee1>
2020-11-06 23:27:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎊 Correct number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '😭 Too high';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if(score > 1){
+      document.querySelector('.message').textContent = '😭 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😱 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = '😩 Too low';
     score--;
