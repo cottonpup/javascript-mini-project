b21526a77fa4c1e2eda8b65eb8679dd8310bc18a
Younggyoung <younggyoung.lee1>
2020-11-10 17:54:24 +0900

    feat: 주사위 던지기, hold 버튼 기능 추가

--- pig-game/README.md
+++ pig-game/README.md
@@ -1,5 +1,18 @@
 # Pig Game
 
-flow chart: representation of everything that can happen in an application. 
+# flow chart
+
+flow chart: representation of everything that can happen in an application.
 
 [flow chart](https://www.diagrams.net/)
+
+# 한 줄 복사
+
+`shift` + `ALT` + 방향키 아래
+
+# prettier 안 먹을 때
+
+View -> Command Palette, and type: Format Document With
+Then Configure Default Formatter... and then choose Prettier - Code formatter.
+
+alt + shift + f
--- pig-game/script.js
+++ pig-game/script.js
@@ -1,1 +1,85 @@
 'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// addEventListener에 있으면 클릭할 때마다 0으로 초기화 되니 전역변수여야 한다.
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // console.log('worked');
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1:
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // console.log(scores[activePlayer]);
+    // console.log(currentScore);
+    document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 10) {
+      // Finish the game
+      playing = false;
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+    scores[activePlayer] = 0;
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+    document.querySelector(`#score--${activePlayer}`).textContent =
+    scores[activePlayer];
+});
