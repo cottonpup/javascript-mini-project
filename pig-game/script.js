a72af2c4f6860ca9bf7e66917890712bd9abeb78
Younggyoung <younggyoung.lee1>
2020-11-10 18:28:36 +0900

    feat: init function

--- pig-game/script.js
+++ pig-game/script.js
@@ -12,15 +12,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-// addEventListener에 있으면 클릭할 때마다 0으로 초기화 되니 전역변수여야 한다.
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function(){
+    // addEventListener에 있으면 클릭할 때마다 0으로 초기화 되니 전역변수여야 한다.
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;   
+    score1El.textContent = 0;      
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -77,9 +89,5 @@ btnHold.addEventListener('click', function () {
 });
 
 btnNew.addEventListener('click', function () {
-    scores[activePlayer] = 0;
-    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
-    document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
-    document.querySelector(`#score--${activePlayer}`).textContent =
-    scores[activePlayer];
+    init();
 });
