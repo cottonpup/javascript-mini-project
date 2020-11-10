9f39210b7f75969e55e0fdc955e7263806b08ae7
Younggyoung <younggyoung.lee1>
2020-11-10 18:38:22 +0900

    :pencil2: Edit player's limit score 10 => 100

--- pig-game/script.js
+++ pig-game/script.js
@@ -75,7 +75,7 @@ btnHold.addEventListener('click', function () {
     document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
