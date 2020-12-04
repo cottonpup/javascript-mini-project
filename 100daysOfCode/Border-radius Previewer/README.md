9b94f0a0d5781608527967a378974d9800a3dabe
Younggyoung <younggyoung.lee1>
2020-12-04 17:25:59 +0900

    Border-radius-previewer Update

---
+++ 100daysOfCode/Border-radius Previewer/README.md
@@ -0,0 +1,6 @@
+# border-radius 8 values
+
+![border-radius 8 values](../../images/2467d58953f702db51342a8788f8d0a5083ff2ce952455a5c7e6790759d9a4b4.png)
+
+“If values are given before and after the slash, then the values before the slash set the horizontal radius and the values after the slash set the vertical radius. If there is no slash, then the
+values set both radii equally.” [W3C](https://www.w3.org/TR/css-backgrounds-3/#border-radius)
--- 100daysOfCode/Border-radius Previewer/script.js
+++ 100daysOfCode/Border-radius Previewer/script.js
@@ -1,4 +1,77 @@
+'use strict';
+
 // User can see a box which has a border-radius property applied to it
 // User can change the 4 border-radius values that are applied to the box (top-left, top-right, bottom-left, bottom-right)
 // User can copy the resulting CSS to the clipboard
 // User can change all 8 possible values of the border-radius in order to create a complex shape
+
+const previewBox = document.querySelector('.preview-box');
+const horizontalTopLeft = document.querySelector('#horizontal-top-left');
+const horizontalTopRight = document.querySelector('#horizontal-top-right');
+const horizontalBottomRight = document.querySelector('#horizontal-bottom-right');
+const horizontalBottomLeft = document.querySelector('#horizontal-bottom-left');
+
+const verticalLeftTop = document.querySelector('#vertical-left-top');
+const verticalRightTop = document.querySelector('#vertical-right-top');
+const verticalRightBottom = document.querySelector('#vertical-right-bottom');
+const verticalLeftBottom = document.querySelector('#vertical-left-bottom');
+
+const result = document.querySelector('input[type="text"]');
+const copy = document.querySelector('input[type="button"]');
+const modal = document.querySelector('.modal');
+
+let $horizontalTopLeft;
+let $horizontalTopRight;
+let $horizontalBottomRight;
+let $horizontalBottomLeft;
+
+let $verticalLeftTop;
+let $verticalRightTop;
+let $verticalRightBottom;
+let $verticalLeftBottom;
+
+copy.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+
+    result.select();
+    result.setSelectionRange(0, 99999);
+
+    document.execCommand('copy');
+
+    if (!modal.classList.contains('hidden')) {
+        window.setTimeout(() => {
+            modal.classList.add('hidden');
+        }, 1000);
+    }
+});
+
+function oninputSet() {
+    horizontalTopLeft.oninput = valueSet;
+    horizontalTopRight.oninput = valueSet;
+    horizontalBottomRight.oninput = valueSet;
+    horizontalBottomLeft.oninput = valueSet;
+
+    verticalLeftTop.oninput = valueSet;
+    verticalRightTop.oninput = valueSet;
+    verticalRightBottom.oninput = valueSet;
+    verticalLeftBottom.oninput = valueSet;
+}
+
+function valueSet() {
+    oninputSet();
+    $horizontalTopLeft = horizontalTopLeft.value;
+    $horizontalTopRight = horizontalTopRight.value;
+    $horizontalBottomRight = horizontalBottomRight.value;
+    $horizontalBottomLeft = horizontalBottomLeft.value;
+
+    $verticalLeftTop = verticalLeftTop.value;
+    $verticalRightTop = verticalRightTop.value;
+    $verticalRightBottom = verticalRightBottom.value;
+    $verticalLeftBottom = verticalLeftBottom.value;
+
+    previewBox.style.borderRadius = `${$horizontalTopLeft}% ${$horizontalTopRight}% ${$horizontalBottomRight}% ${$horizontalBottomLeft}%  
+                                    / ${$verticalLeftTop}% ${$verticalRightTop}% ${$verticalRightBottom}% ${$verticalLeftBottom}%`;
+    result.value = previewBox.style.borderRadius;
+}
+
+valueSet();
