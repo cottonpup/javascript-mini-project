de9513d847130589639a4b6e56d594ca2047efc7
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2020-12-12 00:25:52 +0900

    Update script.js

--- 100daysOfCode/CauseEffect/script.js
+++ 100daysOfCode/CauseEffect/script.js
@@ -10,15 +10,80 @@
 const profileName = document.querySelector('profile');
 const name = document.querySelectorAll('.profile-list-name');
 const modal = document.querySelector('.modal');
+const setFullName = document.querySelector('#full-name');
+const setStreet = document.querySelector('#street');
+const setCity = document.querySelector('#city');
+const setState = document.querySelector('#state');
+const setCountry = document.querySelector('#country');
+const setTelephone = document.querySelector('#telephone-number');
+const setBirthday = document.querySelector('#birthday');
+const setProfileName = document.querySelector('profile');
+const setName = document.querySelectorAll('.profile-list-name');
+const setModal = document.querySelector('.modal');
+const people = [
+    { name: 'James Smith', street: '2130 66th Ave', city: 'California(CA)', state: 'Oakland', country: 'United States', telephone: '(408) 606-5775', birthday: '1987.04.21' },
+    { name: 'Michael Smith', street: '123 11th Ave', city: 'Texas(TX)', state: 'Throckmorton', country: 'China', telephone: '010-323-1234', birthday: '1960.08.12' },
+    { name: 'Maria Rodriguez', street: '561 21th Ave', city: 'Missouri(MO)', state: 'Hazelwood', country: 'Mexico', telephone: '(45) 2313-4342', birthday: '1994.02.24' },
+    { name: 'Ariana Grande-Butera', street: '130 5th Ave', city: 'Georgia(GA)', state: 'Atlanta', country: 'United States', telephone: '134-206-5775', birthday: '2004.11.25' },
+    { name: 'Taylor Alison Swift', street: '1230 1th Ave', city: 'Florida(FL)', state: 'Ponte Vedra Beach', country: 'Japan', telephone: '32-3423-21', birthday: '1997.04.01' }
+];
 
 name.forEach((name) => {
-    name.addEventListener('click', function () {
+    name.addEventListener('click', function (event) {
         profileName.classList.add('left-to-right');
         const activeName = document.querySelector('.profile-list-name.active');
         name.classList.toggle('active');
         modal.classList.add('modal-animation-in');
+        if (activeName === event.target) {
+            modal.classList.remove('modal-animation-in');
+            profileName.classList.remove('left-to-right');
+        }
         if (activeName && activeName !== name) {
             activeName.classList.toggle('active');
+        }
+        for (let num = 0; num < 10; num++) {
+            if (people[num].name.includes(name.innerText)) {
+                setFullName.value = people[num].name;
+                setStreet.value = people[num].street;
+                setCity.value = people[num].city;
+                setState.value = people[num].state;
+                setCountry.value = people[num].country;
+                setTelephone.value = people[num].telephone;
+                setBirthday.value = people[num].birthday;
+            }
+        }
+    });
+});
+
+// save 버튼을 누르면, 이름이 같은 경우에 객체를 변경.
+document.querySelector('#cancel-button').addEventListener('click', function () {
+    name.forEach((event) => {
+        for (let num = 0; num < 10; num++) {
+            if (people[num].includes(event.target.innerText)) {
+                setFullName.value = people[num].name;
+                setStreet.value = people[num].street;
+                setCity.value = people[num].city;
+                setState.value = people[num].state;
+                setCountry.value = people[num].country;
+                setTelephone.value = people[num].telephone;
+                setBirthday.value = people[num].birthday;
+            }
+        }
+    });
+});
+
+document.querySelector('#save-button').addEventListener('click', function () {
+    name.forEach((name) => {
+        for (let num = 0; num < 10; num++) {
+            if (people[num].name.includes(name.innerText)) {
+                people[num].name = setFullName.value;
+                people[num].street = setStreet.value;
+                people[num].city = setCity.value;
+                people[num].state = setState.value;
+                people[num].country = setCountry.value;
+                people[num].telephone = setTelephone.value;
+                people[num].birthday = setBirthday.value;
+            }
         }
     });
 });
