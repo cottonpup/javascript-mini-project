fba686d91ef4f07a5c4e42fc7f02c0b0ff40ba36
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2020-12-13 17:57:27 +0900

    Update script.js

--- 100daysOfCode/CauseEffect/script.js
+++ 100daysOfCode/CauseEffect/script.js
@@ -20,11 +20,51 @@ const setBirthday = document.querySelector('#birthday');
 
 const setModal = document.querySelector('.modal');
 const people = [
-    { name: 'James Smith', street: '2130 66th Ave', city: 'California(CA)', state: 'Oakland', country: 'United States', telephone: '(408) 606-5775', birthday: '1987.04.21' },
-    { name: 'Michael Smith', street: '123 11th Ave', city: 'Texas(TX)', state: 'Throckmorton', country: 'China', telephone: '010-323-1234', birthday: '1960.08.12' },
-    { name: 'Maria Rodriguez', street: '561 21th Ave', city: 'Missouri(MO)', state: 'Hazelwood', country: 'Mexico', telephone: '(45) 2313-4342', birthday: '1994.02.24' },
-    { name: 'Ariana Grande-Butera', street: '130 5th Ave', city: 'Georgia(GA)', state: 'Atlanta', country: 'United States', telephone: '134-206-5775', birthday: '2004.11.25' },
-    { name: 'Taylor Alison Swift', street: '1230 1th Ave', city: 'Florida(FL)', state: 'Ponte Vedra Beach', country: 'Japan', telephone: '32-3423-21', birthday: '1997.04.01' }
+    {
+        name: 'James Smith',
+        street: '2130 66th Ave',
+        city: 'California(CA)',
+        state: 'Oakland',
+        country: 'United States',
+        telephone: '(408) 606-5775',
+        birthday: '1987.04.21'
+    },
+    {
+        name: 'Michael Smith',
+        street: '123 11th Ave',
+        city: 'Texas(TX)',
+        state: 'Throckmorton',
+        country: 'China',
+        telephone: '010-323-1234',
+        birthday: '1960.08.12'
+    },
+    {
+        name: 'Maria Rodriguez',
+        street: '561 21th Ave',
+        city: 'Missouri(MO)',
+        state: 'Hazelwood',
+        country: 'Mexico',
+        telephone: '(45) 2313-4342',
+        birthday: '1994.02.24'
+    },
+    {
+        name: 'Ariana Grande-Butera',
+        street: '130 5th Ave',
+        city: 'Georgia(GA)',
+        state: 'Atlanta',
+        country: 'United States',
+        telephone: '134-206-5775',
+        birthday: '2004.11.25'
+    },
+    {
+        name: 'Taylor Alison Swift',
+        street: '1230 1th Ave',
+        city: 'Florida(FL)',
+        state: 'Ponte Vedra Beach',
+        country: 'Japan',
+        telephone: '32-3423-21',
+        birthday: '1997.04.01'
+    }
 ];
 const addButton = document.querySelector('#add-button');
 const newPersonInput = document.querySelector('#new-person-input');
@@ -68,6 +108,51 @@ document.querySelector('#save-button').addEventListener('click', function () {
     }
 });
 
+const activeEvent = (event) => {
+    console.log(`노드리스트 길이는 ${names.length}`);
+    //   console.log(`지금 클릭한 이름은 ${name.innerText}`);
+
+    const activeName = document.querySelector('.profile-list-name.active');
+
+    profileName.classList.add('name-in-and-out');
+    modal.classList.add('modal-animation-in');
+    event.target.classList.toggle('active');
+
+    if (modal.classList.contains('modal-animation-in')) addButton.classList.remove('hidden');
+
+    if (activeName && activeName !== event.target) activeName.classList.toggle('active');
+
+    if (activeName === event.target) {
+        profileName.classList.remove('name-in-and-out');
+        modal.classList.remove('modal-animation-in');
+        addButton.classList.add('hidden');
+    }
+
+    //   for (let num = 0; num < people.length; num++) {
+    //     if (people[num].name.includes(name.innerText)) {
+    //       init(num);
+    //     }
+    //   }
+};
+
+const addEvent = () => {
+    [...names].forEach((name) => name.addEventListener('click', activeEvent));
+};
+
+addEvent();
+
+const defaultObject = {
+    name: `${newPersonInput.value}`,
+    street: '',
+    city: '',
+    state: '',
+    country: '',
+    telephone: '',
+    birthday: ''
+};
+// button 의 active, hidden 을 담당하는 함수
+
+// button 에 입력하면 새로 목록을 추가
 addButton.addEventListener('click', function () {
     addButton.value = 'Save';
     addButton.classList.toggle('active');
@@ -76,10 +161,12 @@ addButton.addEventListener('click', function () {
     if (!addButton.classList.contains('active') && modal.classList.contains('modal-animation-in')) {
         document.querySelector('.profile-list').insertAdjacentHTML('beforeend', `<h1 class='profile-list-name'>${newPersonInput.value}</h1>`);
         addButton.value = 'Add new person';
-        const defaultObject = { name: `${newPersonInput.value}`, street: '', city: '', state: '', country: '', telephone: '', birthday: '' };
+
         people.push(defaultObject);
         console.log(people);
+
         newPersonInput.value = '';
+        addEvent();
     }
 });
 
@@ -89,27 +176,3 @@ addButton.addEventListener('click', function () {
 
 객체에 true/false 추가해서 애니메이션. 
 */
-
-[...names].forEach((name) => {
-    name.addEventListener('click', function (event) {
-        console.log(`노드리스트 길이는 ${names.length}`);
-        console.log(`지금 클릭한 이름은 ${name.innerText}`);
-        const activeName = document.querySelector('.profile-list-name.active');
-        profileName.classList.add('name-in-and-out');
-        modal.classList.add('modal-animation-in');
-        event.target.classList.toggle('active');
-
-        if (modal.classList.contains('modal-animation-in')) addButton.classList.remove('hidden');
-        if (activeName && activeName !== event.target) activeName.classList.toggle('active');
-        if (activeName === event.target) {
-            profileName.classList.remove('name-in-and-out');
-            modal.classList.remove('modal-animation-in');
-            addButton.classList.add('hidden');
-        }
-        for (let num = 0; num < people.length; num++) {
-            if (people[num].name.includes(name.innerText)) {
-                init(num);
-            }
-        }
-    });
-});
