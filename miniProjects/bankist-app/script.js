3f9d8a66888e374e00dad9e74f00622c76d85840
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-01-07 16:38:57 +0900

    Edited Bankist App CSS, Script files

--- miniProjects/bankist-app/script.js
+++ miniProjects/bankist-app/script.js
@@ -74,3 +74,131 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const displayMovements = function(movements){
+  containerMovements.innerHTML = '';
+  movements.forEach(function(mov, i){
+    const type = mov > 0? 'deposit':'withdrawal';
+    const html = `
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">
+          ${i + 1} ${type}
+        </div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+displayMovements(account1.movements);
+
+const calcPrintBalance = function(movements){
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+}
+
+calcPrintBalance(account1.movements);
+
+const createUserName = function(accs){
+  accs.forEach(function(acc){
+    acc.userName = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map(name => name[0])
+    .join('');
+  })
+}
+
+createUserName(accounts);
+console.log(accounts);
+
+
+const eurToUsd = 1.1;
+// const movementsUSD = movements.map(function(mov){
+//     return mov * eurToUsd;
+// });
+// // 화살표 함수로 변경하기 🏹 
+// // 한 줄이면 return 키워드 불필요
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementUSDfor = [];
+// for(const mov of movements){
+//   movementUSDfor.push(mov * eurToUsd);
+// }
+// console.log(movementUSDfor);
+
+// Map으로 새로운 배열로 출력하기 🗺
+// movements.map(function(mov, i){
+//   if(mov > 0){
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`)
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// })
+
+movements.map((mov, i) => console.log(`Movement ${i + 1}: You ${mov > 0? 'deposited' : 'withdrew'} ${Math.abs(mov)}`))
+
+// Lectures 👩🏻‍💻👨🏻‍💻
+
+const deposit = movements.filter(function(mov){
+  return mov > 0;
+})
+console.log(deposit);
+
+const depositFor = [];
+for(const mov of movements) if(mov > 0) depositFor.push(mov);
+console.log(depositFor);
+
+const withdrawals = movements.filter((mov) => mov < 0)
+console.log(withdrawals);
+
+// REDUCE METHOD
+// accumulator -> SNOWBALL ☃️
+const balance = movements.reduce(function(acc, cur, i, arr){ // acc = accumulator
+  console.log(`acc: ${acc} // cur: ${cur}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//  acc: 0 // cur: 200
+//  acc: 200 // cur: 450
+//  acc: 650 // cur: -400
+//  acc: 250 // cur: 3000
+//  acc: 3250 // cur: -650
+//  acc: 2600 // cur: -130
+//  acc: 2470 // cur: 70
+//  acc: 2540 // cur: 1300
+
+
+// FOR OF 활용하기 - 초기값(쌓이는 값)은 유동적으로 계속 변하니까 let으로 설정해두어야 함! 
+let balance2 = 0;
+for(const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+// const max = movements.reduce((acc, mov) => {
+//   if(acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+
+
+// Maximum value
+console.log(movements);
+const max = movements.reduce((acc, mov) => {
+  if(acc > mov) {
+    console.log(`${acc} > ${mov}`);
+    return acc
+  } else {
+    console.log(`${acc} < ${mov}`);
+    return mov
+  };
+}, movements[0]); // initialValue에 0하지 않기! 만약 값들이 다 마이너스면 어떡해..ㅎㅎ
+
+console.log(`최후의 승자..👑: ${max}`);
+
